11881c15f833a91316056473d111a670136d0fca
hellen0874 <en.musinova@gmail.com>
2022-09-04 22:48:06 +0300

    Revert "Class and object added, changing for homework 3"
    This reverts commit 8b371687c1bec656d9e257d81b34b1b25d7a40b2.

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        BonusMilesService service = new BonusMilesService();
-        int price = 10_000;
-        int miles = service.calculate(price);
-        System.out.println(miles);
+
+        int prise = 5000; // Стоимость билета
+        int roublesPerMile = 20; // кол-во рублей на бонусную милю
+        int bonusMiles = prise / roublesPerMile;
+
+        System.out.println("Бонус равен " + bonusMiles);
     }
 }
-
-// изначальный вариант на всякий случай
-//public static void main(String[] args) {
-
-// int prise = 5000; // Стоимость билета
-// int roublesPerMile = 20; // кол-во рублей на бонусную милю
-// int bonusMiles = prise / roublesPerMile;
-
-//System.out.println("Бонус равен " + bonusMiles);
-// }
-// }
